dcc07563ccf5a973793b0eec7b129969b7e01ce2
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-11-14 15:00:53 -0500

    listo

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-//CAMBIAR LA PERSPECTIVA DEL PERSONAJE/////////////////////////
+//CAMBIAR LA PERSPECTIVA DEL PERSONAJE///////////////////////////////////
 const basePrincipal = document.getElementById('base')
 
-///cuando se hace click ////////////////////////
+///cuando se hace click ----------------------
 window.addEventListener('mousedown', () => {
   window.addEventListener('mousemove', girar)
 })
 
-///cuando se deja de hacer click ///////////////
+///cuando se deja de hacer click -------------
 window.addEventListener('mouseup', () => {
   removeEventListener('mousemove', girar)
 })
 
 //--------------------------------------------
 function girar(evento) {
-
   //obtener la coordenadas del eje X y Y de la flecha de mouse
   let X = evento.clientX
   let Y = evento.clientY
@@ -22,8 +21,7 @@ function girar(evento) {
   basePrincipal.style.transform = `rotateX(-${Y}deg) rotateY(${X}deg)`
 }
 
-
-//MODO AGACHADO////////////////////////////////////////////////
+//MODO AGACHADO////////////////////////////////////////////////////////////
 window.addEventListener('dblclick', agachar)
 
 let cabeza = document.getElementById('cabeza')
