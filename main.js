46cf2d5c1d97cfd852114ae7ab1e41fbe90b8a6e
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-11-14 14:35:59 -0500

    funcion de poder agacahrse con doble click terminado

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//CAMBIAR LA PERSPECTIVA DEL PERSONAJE/////////////////////////
 const basePrincipal = document.getElementById('base')
 
 ///cuando se hace click ////////////////////////
@@ -10,8 +11,7 @@ window.addEventListener('mouseup', () => {
   removeEventListener('mousemove', girar)
 })
 
-
-/////////////////////////////////////////////////
+//--------------------------------------------
 function girar(evento) {
 
   //obtener la coordenadas del eje X y Y de la flecha de mouse
@@ -23,3 +23,23 @@ function girar(evento) {
 }
 
 
+//MODO AGACHADO////////////////////////////////////////////////
+window.addEventListener('dblclick', agachar)
+
+let cabeza = document.getElementById('cabeza')
+let tronco = document.getElementById('tronco')
+let brazoIzquierdo = document.getElementById('brazo-izquierdo')
+let brazoDerecho = document.getElementById('brazo-derecho')
+let piernaIzquierda = document.getElementById('pierna-izquierda')
+let piernaDerecha = document.getElementById('pierna-derecha')
+
+function agachar() {
+  cabeza.classList.toggle('cabeza--agachado')
+  tronco.classList.toggle('tronco--agachado')
+  brazoIzquierdo.classList.toggle('brazo-izquierdo--agachado')
+  brazoDerecho.classList.toggle('brazo-derecho--agachado')
+  piernaIzquierda.classList.toggle('pierna-izquierda--agachado')
+  piernaDerecha.classList.toggle('pierna-derecha--agachado')
+}
+
+agachar()
